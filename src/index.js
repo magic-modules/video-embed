4f35e6b142f93814d2089820a166ac68fba6eae1
Jascha <jascha@jaeh.at>
2019-04-28 02:18:19 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,60 +1,63 @@
-const Youtube = p => {
-  if (typeof p === 'string') {
+const VideoEmbed = p => {
+  if (typeof p !== 'object') {
     p = {
       src: p,
     }
   }
 
-  let { src, width, height, ...props } = p
-
-  if (!props.class || !props.class.includes('Youtube')) {
-    props.class = `Youtube${props.class ? ` ${props.class}` : ''}`
-  }
-
+  let { src, width, height, class: cl = 'VideoEmbed', style = '', host, ...props } = p
   // if there is no video url, why would we render?
   if (!src) {
     return
   }
 
-  // if the props.src string includes youtube, we just assume the url to be complete.
-  src = src.includes('youtube') ? src : `https://www.youtube-nocookie.com/embed/${src}`
-
-  props.style = props.style || ''
-  if (width) {
-    props.style += `width: ${width};`
+  if (!cl.includes('VideoEmbed')) {
+    cl = `VideoEmbed ${cl}`
   }
 
+  // src is numeric, assume vimeo
+  if (src === +src) {
+    src = `https://player.vimeo.com/video/${src}`
+  } else if (!src.startsWith('http')) {
+    src = `https://www.youtube-nocookie.com/embed/${src}`
+  }
+
+  if (width) {
+    style += `width: ${width};`
+  }
   if (height) {
-    props.style += `height: ${height};`
+    style += `height: ${height};`
   }
 
   return div(
-    props,
+    {
+      class: cl,
+      style,
+    },
     iframe({
       frameborder: 0,
-      allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture',
+      allow: 'encrypted-media',
       allowfullscreen: '',
       src,
+      props,
     }),
   )
 }
 
-Youtube.style = {
-  '.Youtube': {
-    height: 0,
-    overflow: 'hidden',
-    paddingBottom: '56.25%',
-    paddingTop: '30px',
-    position: 'relative',
+VideoEmbed.style = {
+  height: 0,
+  overflow: 'hidden',
+  paddingBottom: '56.25%',
+  paddingTop: '30px',
+  position: 'relative',
 
-    iframe: {
-      height: '100%',
-      left: 0,
-      position: 'absolute',
-      top: 0,
-      width: '100%',
-    },
+  iframe: {
+    height: '100%',
+    left: 0,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
   },
 }
 
-module.exports = Youtube
+module.exports = VideoEmbed
