74c64362834c397bdd7c17e6d27306bf104312e7
Jascha <jascha@jaeh.at>
2019-05-06 17:19:15 +0200

    format

--- example/app.js
+++ example/app.js
@@ -3,11 +3,15 @@ module.exports = {
     logotext: 'VideoEmbed',
     menu: [
       { to: '/#installation', text: 'installation' },
-      { to: '/#usage', text: 'usage', items: [
-        { to: '/#usage-youtube', text: 'youtube' },
-        { to: '/#usage-vimeo', text: 'vimeo' },
-        { to: '/#usage-others', text: 'other hosts' },
-      ] },
+      {
+        to: '/#usage',
+        text: 'usage',
+        items: [
+          { to: '/#usage-youtube', text: 'youtube' },
+          { to: '/#usage-vimeo', text: 'vimeo' },
+          { to: '/#usage-others', text: 'other hosts' },
+        ],
+      },
       { to: '/#props', text: 'props' },
       { to: '/#fixed-dimensions', text: 'fixed dimensions' },
       { to: '/#source', text: 'source' },
